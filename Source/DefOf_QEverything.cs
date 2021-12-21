9d94a8fde70681e1e78c2fb1df6338379a0378f0
Cozarkian <77899393+Cozarkian@users.noreply.github.com>
2021-12-20 16:42:51 -0800

    Add files via upload

---
+++ Source/DefOf_QEverything.cs
@@ -0,0 +1,22 @@
+﻿using RimWorld;
+
+namespace QualityEverything
+{
+    [DefOf]
+    public static class DefOf_QEverything
+    {
+        public static InspirationDef QF_Inspired_Butchering;
+        public static InspirationDef QF_Inspired_Chemistry;
+        public static InspirationDef QF_Inspired_Construction;
+        public static InspirationDef QF_Inspired_Cooking;
+        public static InspirationDef QF_Inspired_Gathering;
+        public static InspirationDef QF_Inspired_Harvesting;
+        public static InspirationDef QF_Inspired_Mining;
+        public static InspirationDef QF_Inspired_Stonecutting;
+
+        static DefOf_QEverything()
+        {
+            DefOfHelper.EnsureInitializedInCtor(typeof(DefOf_QEverything));
+        }
+    }
+}
